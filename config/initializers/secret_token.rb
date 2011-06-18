15258315a4260e2180f376ee4f9510aeb9489d8a
danteata <dan.abakah@gmail.com>
2011-06-18 00:39:32 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MestNetwork::Application.config.secret_token = 'ef29578f8c4b7f72c2162184df499e1e189e175b038b75b5e15a611ebf3ee179d055d63d4f20abe42318d9fe82e648351491ded72bb2415b303f5041fb1e6f04'
